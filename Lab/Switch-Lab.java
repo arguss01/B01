a31643e799b92fd7542ab6bd74856ef21970bab0
Lucas Dias <168231715+arguss01@users.noreply.github.com>
2024-09-16 10:36:30 -0300

    Update Switch-Lab.java

--- Lab/Switch-Lab.java
+++ Lab/Switch-Lab.java
@@ -1,1 +1,21 @@
-
+import java.util.Scanner;
+public class SwitchLab {
+        public static void main(String [] args) {
+            //declaração de variavel
+            int opcao;
+            //cria objeto para entrada da opção do cardapio
+            Scanner leitorOpcao = new Scanner(System.in);
+            //apresenta o cardapio
+            System.out.println(x:"1 - Pastel de carne \n");
+            System.out.println(x:"2 - coxinha de frango \n");
+            System.out.println(x:"3 - kibe com queijo \n");
+            //entrada da opção do cardapio
+            System.out.print(s:"Escolha uma opção: ");
+            opcao = leitorOpcao.nextInt();
+            //seleção da opção
+            switch(opcao)
+            {
+                case 1
+            }
+        }
+}
